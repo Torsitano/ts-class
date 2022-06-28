927f62998550ea2b6aabe6ae6db82d8fbc01cd67
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-06-28 06:50:25 -0400

    readme

--- sort/src/CharactersCollection.ts
+++ sort/src/CharactersCollection.ts
@@ -12,12 +12,16 @@ export class CharactersCollection implements Sortable {
     }
 
     compare(leftIndex: number, rightIndex: number): boolean {
-        
+        return this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
 
-        return false
     }
 
     swap(leftIndex: number, rightIndex: number): void {
-        
+        const characters = this.data.split('')
+        const leftHand = characters[leftIndex]
+        characters[leftIndex] = characters[rightIndex]
+        characters[rightIndex] = leftHand
+
+        this.data = characters.join('')
     }
 }
