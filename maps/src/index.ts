c821c9df389c8e2f70f7c9af87f15312c9100405
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-07-06 07:33:40 -0400

    updates

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,12 +1,21 @@
-import { Company } from './Company';
-import { User } from './User'
-import { CustomMap } from "./CustomMap";
+new google.maps.Map( document.getElementById( 'map' ) as HTMLElement, {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+} )
 
-const divId = 'map'
 
-const customMap = new CustomMap(divId)
-const user = new User()
-const company = new Company()
+// import { Company } from './Company';
+// import { User } from './User'
+// import { CustomMap } from "./CustomMap";
 
-customMap.addMarker(user)
-customMap.addMarker(company)
+// const divId = 'map'
+
+// const customMap = new CustomMap(divId)
+// const user = new User()
+// const company = new Company()
+
+// customMap.addMarker(user)
+// customMap.addMarker(company)
